f96f86b3a718154aac52876b24c0f190ee361a1d
Vincent <vincentxavier2602@gmail.com>
2023-02-23 19:29:48 +0530

    On Scroll Event Update

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,11 +1,36 @@
-import URLRoutes from './Components/URLRouting/Routes';
-import 'react-toastify/dist/ReactToastify.css';
-import '@animxyz/core'
-
-
+import URLRoutes from "./Components/URLRouting/Routes";
+import "react-toastify/dist/ReactToastify.css";
+import "@animxyz/core";
+import "./index.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [scroll, setScroll] = useState(0);
+  console.log(scroll);
+  useEffect(() => {
+    let progressBarHandler = () => {
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scroll = `${totalScroll / windowHeight}`;
+
+      setScroll(scroll);
+    };
+
+    window.addEventListener("scroll", progressBarHandler);
+
+    return () => window.removeEventListener("scroll", progressBarHandler);
+  });
   return (
-    <URLRoutes />
+    <>
+      <div id="progressBarContainer">
+        <div
+          id="progressBar"
+          style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll}` }}
+        />
+      </div>
+      <URLRoutes />
+    </>
   );
 }
 
